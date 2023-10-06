9f434e4b74d972c873d3735c667629d30cc9ae0a
Wanangwa <Martinnyirenda8@gmail.com>
2023-10-06 09:03:39 +0000

    Created Searchbar component

---
+++ components/Home/PropertyCard.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function PropertyCard() {
+  return (
+    <Link
+      href="https://i.ibb.co/xDT3Q4y/scott-webb-1ddol8rg-UH8-unsplash.jpg"
+      className="w-90 h-22 rounded shadow-lg"
+    >
+      Hello
+    </Link>
+  );
+}
